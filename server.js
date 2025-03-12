66d1d57b68fe46b329f32b58bcc615f2236cc197
Akash-Softw <akashofficial881@gmail.com>
2025-03-12 23:28:11 +0530

    again check error 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.us(cors());
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files, if any
 app.set('view engine', 'ejs');
